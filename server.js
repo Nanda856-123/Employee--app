2c1555a301e107dde59dae50c4ad2ffcdf00fcd7
Nanda <nandaammu9@gmail.com>
2025-10-30 12:03:39 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts'); 
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json()); 
+app.use(methodOverride('_method')); 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(expressLayouts);
+app.set('layout', 'layout'); 
+
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log(' MongoDB connected'))
+.catch(err => console.error(' MongoDB connection error:', err));
+
+
+const employeeRoutes = require('./routes/employees');
+app.use('/', employeeRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
